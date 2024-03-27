72adeb1fc89c202c5376e7cf6b6c5338f4ac637d
mAngelo-dev <m.angelodev@gmail.com>
2024-03-27 15:45:31 -0300

    V. 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Assuming you're using React Router for navigation
+import { useAuthContext } from '@asgardeo/auth-react';
 import './App.css';
 
 function App() {
+  const navigate = useNavigate(); // Antigo useHistory, utilizado para redirecionamento de rotas.
+  const { state, signIn } = useAuthContext(); //  Resgata estado de autenticação e função de login
+
+  useEffect(() => {
+    // Redireciona para o dashboard caso o usuário esteja autenticado
+    if (state.isAuthenticated) {
+      navigate('/dashboard');
+    }
+  }, [state.isAuthenticated, navigate]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>SSO WSO2 Identity Server</h1>
+      {!state.isAuthenticated && (
+        <button className="login-button" onClick={ () =>  signIn()}>
+          Login
+        </button>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@asgardeo/auth-react";
+import config from "./config.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Use createRoot instead of ReactDOM.render
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider config={config}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
